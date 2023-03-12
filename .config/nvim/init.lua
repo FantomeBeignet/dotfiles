6c35dab81dd4ef73c9107a5875989f2a7b59a4fd
FantomeBeignet <himself@fantomebeig.net>
2023-03-12 13:57:35 +0100

    [~] telescope ignore git directory

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -197,6 +197,7 @@ vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
@@ -278,7 +279,12 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>sf', function() require('telescope.builtin').find_files({hidden = true}) end, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', function()
+  -- Show hidden files
+  require('telescope.builtin').find_files({
+    find_command = {'rg', '--files', '--color', 'never', '--iglob', '!.git', '--hidden'}
+  })
+end, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })

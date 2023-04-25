ab7b382761de06cc10edddcb234670ee0a499390
Tom Béné <himself@fantomebeig.net>
2023-04-25 13:08:20 +0200

    removed help ts parser

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -390,7 +390,7 @@ vim.keymap.set('n', '<leader>hr', function() hui.nav_file(4) end, { desc = 'Mark
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = true,

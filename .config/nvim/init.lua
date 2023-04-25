cde18429abdc87434080e2b6e4c8ac7e6aa82edc
Tom Béné <himself@fantomebeig.net>
2023-04-25 15:07:16 +0200

    added cokeline as buffer line

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -225,6 +225,99 @@ require('lazy').setup({
   },
 
   {
+    'willothy/nvim-cokeline',
+    dependencies = { 'kyazdani42/nvim-web-devicons' },
+    config = function ()
+      local get_hex = require('cokeline.utils').get_hex
+      local normal_fg = get_hex('Normal', 'fg')
+      local comments_fg = get_hex('Comment', 'fg')
+      local errors_fg = get_hex('DiagnosticSignError', 'fg')
+      local warnings_fg = get_hex('DiagnosticSignWarn', 'fg')
+      local background_bg = get_hex('Background', 'bg')
+      local components = {
+        space = {
+          text = ' ',
+          truncation = { priority = 1 }
+        },
+        two_spaces = {
+          text = '  ',
+          truncation = { priority = 1 },
+        },
+        separator = {
+          text = function(buffer)
+            return buffer.index ~= 1 and '▏' or ''
+          end,
+          truncation = { priority = 1 }
+        },
+        devicon = {
+          text = function (buffer)
+            return buffer.devicon.icon
+          end,
+          fg = function (buffer)
+            return buffer.devicon.color
+          end,
+          truncation = { priority = 1 },
+        },
+        filename = {
+          text = function(buffer)
+            return buffer.filename
+          end,
+          truncation = {
+            priority = 2,
+            direction = 'left',
+          },
+        },
+        diagnostics = {
+          text = function(buffer)
+            return
+              (buffer.diagnostics.errors ~= 0 and '  ' .. buffer.diagnostics.errors)
+              or (buffer.diagnostics.warnings ~= 0 and '  ' .. buffer.diagnostics.warnings)
+              or ''
+          end,
+          fg = function(buffer)
+            return
+              (buffer.diagnostics.errors ~= 0 and errors_fg)
+              or (buffer.diagnostics.warnings ~= 0 and warnings_fg)
+              or nil
+          end,
+          truncation = { priority = 1 },
+        },
+        close_or_unsaved = {
+          text = function(buffer)
+            return buffer.is_modified and '  ●' or ''
+          end,
+          fg = function(buffer)
+            return buffer.is_modified and comments_fg or nil
+          end,
+          truncation = { priority = 1 },
+        },
+      }
+      require('cokeline').setup({
+        default_hl = {
+          fg = function(buffer)
+            return
+              buffer.is_focused
+              and normal_fg
+              or comments_fg
+          end,
+          bg = background_bg,
+        },
+        components = {
+          components.space,
+          components.separator,
+          components.space,
+          components.devicon,
+          components.space,
+          components.filename,
+          components.diagnostics,
+          components.close_or_unsaved,
+          components.space,
+        },
+      })
+    end
+  },
+
+  {
     'FantomeBeignet/ghostly.nvim',
     config = function()
       vim.api.nvim_command('colorscheme ghostly')

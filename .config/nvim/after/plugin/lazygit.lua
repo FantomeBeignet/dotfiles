6bac94c480b012d3f0a3616413d016ce3b7bc508
Tom Béné <himself@fantomebeig.net>
2023-06-06 01:05:03 +0200

    lazygit and neovim integration

---
+++ .config/nvim/after/plugin/lazygit.lua
@@ -0,0 +1,8 @@
+vim.g.lazygit_floating_window_scaling_factor = 0.8
+
+vim.g.lazygit_use_custom_config_file_path = 1 -- config file path is evaluated if this value is 1
+vim.g.lazygit_config_file_path = "/home/tom/.config/lazygit/config.yml" -- custom config file path
+
+vim.keymap.set("n", "<leader>g", function()
+	require("lazygit").lazygit()
+end, { desc = "Open Lazygit" })

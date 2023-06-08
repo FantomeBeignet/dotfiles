af59b203be95b6c88762cd8122e315825badff2d
Tom Béné <himself@fantomebeig.net>
2023-06-08 20:39:32 +0200

    better scrollbar

--- .config/nvim/after/plugin/lazygit.lua
+++ .config/nvim/after/plugin/lazygit.lua
@@ -1,4 +1,5 @@
 vim.g.lazygit_floating_window_scaling_factor = 0.8
+vim.g.lazygit_floating_window_border_chars = { "", "", "", "", "", "", "", "" }
 
 vim.g.lazygit_use_custom_config_file_path = 1 -- config file path is evaluated if this value is 1
 vim.g.lazygit_config_file_path = "/home/tom/.config/lazygit/config.yml" -- custom config file path
--- .config/nvim/after/plugin/scrollbar.lua
+++ .config/nvim/after/plugin/scrollbar.lua
@@ -1,5 +1,20 @@
 require("scrollbar").setup({
+	hide_if_all_visible = true,
 	handle = {
 		blend = 10,
+	},
+	excluded_buftypes = {
+		"lazygit",
+		"prompt",
+		"terminal",
+	},
+	excluded_filetypes = {
+		"cmp_docs",
+		"cmp_menu",
+		"gitcommit",
+		"lazygit",
+		"noice",
+		"prompt",
+		"TelescopePrompt",
 	},
 })
